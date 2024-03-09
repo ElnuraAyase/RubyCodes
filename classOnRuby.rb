7500fb2d45c0c764cb76fcaf110a9212d83e3bf0
Elnura <elya.ayase@gmail.com>
2024-03-09 23:59:44 +0400

    Update classOnRuby.rb

--- classOnRuby.rb
+++ classOnRuby.rb
@@ -1,1 +1,16 @@
+# Class definition
+class Dog
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+  
+  def bark
+    puts "Woof! My name is #{@name} and I'm a #{@breed}."
+  end
+end
+
+# Create an object (instance) of the class
+my_dog = Dog.new("Buddy", "Labrador")
+my_dog.bark
 
